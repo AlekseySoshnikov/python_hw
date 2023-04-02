5e77639070ceadfb60f45edfabc58698029921cd
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-04-02 15:24:56 +0300

    modified Task-36.py

--- Task-36.py
+++ Task-36.py
@@ -11,11 +11,6 @@
 # 5  10 15 20 25 30
 # 6  12 18 24 30 36
 
-#for i in range(1, num_rows + 1):
-#        for j in range(1, num_columns + 1):
-#            print(*list(map(operation, [i], [j])), end="\t")
-#        print()
-
 def print_operation_table(operation, num_rows=6, num_columns=6):    
     for _ in range(num_rows):
         print(*[[(operation)(i, j) for i in range(1, num_columns + 1)] for j in range(1, num_rows + 1)][_]) 
